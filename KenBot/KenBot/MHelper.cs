9531e28622ebef67de6e6b00f3bb10cfac654488
moeyyas <moeyyas@gmail.com>
2016-07-16 13:37:17 +0300

    Twitch chat monitoring bot.
    A .NET twitch bot developed in C#.

---
+++ KenBot/KenBot/MHelper.cs
@@ -0,0 +1,16 @@
+﻿namespace KenBot
+{
+    public class MHelper
+    {
+        /*public string[] FillModerators(string _NoticeResponse)
+        {
+
+        }*/
+        public class MOTD
+        {
+            public string Message = "Welcome to our channel!";
+            public int Frequency = 600; //In seconds
+            public bool Enabled = true;
+        }
+    }
+}
